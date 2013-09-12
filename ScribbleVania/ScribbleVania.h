fd9cc25cd5b9d755263926947466cccea6c2bcbc
Matthew Hart <mshart@k-state.edu>
2013-09-12 12:22:34 -0500

    First Commit

---
+++ ScribbleVania/ScribbleVania.h
@@ -0,0 +1,46 @@
+// Programming 2D Games
+// Copyright (c) 2011 by: 
+// Charles Kelly
+// Chapter 5 ScribbleVania.h v1.0
+
+#ifndef _SCRIBBLE_VANIA_H             // Prevent multiple definitions if this 
+#define _SCRIBBLE_VANIA_H             // file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "game.h"
+#include "textureManager.h"
+#include "image.h"
+
+//=============================================================================
+// This class is the core of the game
+//=============================================================================
+class ScribbleVania : public Game
+{
+private:
+    // game items
+    TextureManager nebulaTexture;   // nebula texture
+    TextureManager planetTexture;   // planet texture
+    TextureManager shipTexture;     // ship texture
+    Image   planet;                 // planet image
+    Image   nebula;                 // nebula image
+    Image   ship;                   // ship image
+
+
+public:
+    // Constructor
+    ScribbleVania();
+
+    // Destructor
+    virtual ~ScribbleVania();
+
+    // Initialize the game
+    void initialize(HWND hwnd);
+    void update();      // must override pure virtual from Game
+    void ai();          // "
+    void collisions();  // "
+    void render();      // "
+    void releaseAll();
+    void resetAll();
+};
+
+#endif
